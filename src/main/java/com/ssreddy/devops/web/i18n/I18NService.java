05fe1027ad01fbceb43588e8752316d8874f786f
Shyamsundar <tshyam.java@gmail.com>
2019-11-09 08:41:47 +0530

    add loggers in spring boot

--- src/main/java/com/ssreddy/devops/web/i18n/I18NService.java
+++ src/main/java/com/ssreddy/devops/web/i18n/I18NService.java
@@ -1,5 +1,7 @@
 package com.ssreddy.devops.web.i18n;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContext;
@@ -11,10 +13,13 @@ import java.util.Locale;
 @Service
 public class I18NService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(I18NService.class);
+
     @Autowired
     private MessageSource messageSource;
 
     public String getMessage(String messageId) {
+        LOG.info("Return i18n text for messageId {}", messageId);
         Locale locale = LocaleContextHolder.getLocale();
         return getMessage(messageId, locale);
     }
